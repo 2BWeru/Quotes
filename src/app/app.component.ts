5910fb9e378401190b2ad1004ff1ca6baeb2d857
Wamaitha <wamaithaweru19@gmail.com>
2022-04-08 22:04:59 +0300

    delets class display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'Quotes by Wammy'
 
-   quotes!: string[];
+  //  quotes!: string[];
 
-   constructor(){
-     this.quotes = ['People often say that motivation doesnt', 'last Well neither does bathing', 'that is why we recommend it daily.' ,'Zig Ziglar']
-   }
+  //  constructor(){
+  //    this.quotes = ['People often say that motivation doesnt', 'last Well neither does bathing', 'that is why we recommend it daily.' ,'Zig Ziglar']
+  //  }
+ 
+
+
+  ngOnInit(): void {
+  }
 }
