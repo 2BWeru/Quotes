4e4c4808f92e79feb788882ae67a6467d2c4c672
Wamaitha <wamaithaweru19@gmail.com>
2022-04-08 21:14:20 +0300

    cretes an  arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes by Wammy';
+  title = 'Quotes by Wammy'
+
+   quotes!: string[];
+
+   constructor(){
+     this.quotes = ['People often say that motivation doesnt', 'last Well neither does bathing', 'that is why we recommend it daily.' ,'Zig Ziglar']
+   }
 }
