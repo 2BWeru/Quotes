5480398aca6a82931c23374af5df9e462f882fc0
Wamaitha <wamaithaweru19@gmail.com>
2022-04-10 06:55:02 +0300

    input class propeties

--- src/app/quote-class.model.ts
+++ src/app/quote-class.model.ts
@@ -1,2 +1,21 @@
 export class QuoteClass {
+    public about!:string;
+    public name! : string;
+    public aurthor!:string;
+
+constructor(about: string, name:string, aurthor: string) {
+    this.about = about;
+    this.name = name;
+    this.aurthor = aurthor;
+  }
+  getAbout(): string {
+    return `${this.about}`;
+  }
+  getName(): string {
+    return `${this.name}`;
+  }
+  getAurthor(): string {
+    return `${this.aurthor}`;
+
+
 }
